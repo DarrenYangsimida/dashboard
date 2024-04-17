95ccff37493d7b096878046ea0ca57009f893fc4
yangli <544052475@qq.com>
2024-04-17 15:45:59 +0800

    Debug 应用中心部署应用切换 tag 识别错误问题

--- apps/kubegems/src/views/resource/appmanifest/components/DeployApp.vue
+++ apps/kubegems/src/views/resource/appmanifest/components/DeployApp.vue
@@ -1,17 +1,17 @@
 <!--
  * Copyright 2022 The kubegems.io Authors
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
 -->
 
 <template>
@@ -185,13 +185,17 @@
         this.environmentAppList();
       },
       splitImage(image, type) {
-        const match = new RegExp('([\\w|/|\\.|-]+)?[:|@]?([\\w|\\.|-]+)?', 'g').exec(image);
+        //const match = new RegExp('([\\w|/|\\.|-]+)?[:|@]?([\\w|\\.|-]+)?', 'g').exec(image);
+        const match = image.split(':');
 
         if (match) {
           if (type === 'image') {
-            return match[1];
+            //return match[1];
+            const imageStr = image.replace(':' + match[match.length - 1], '');
+            return imageStr;
           } else if (type === 'tag') {
-            return match[2];
+            //return match[2];
+            return match[match.length - 1];
           }
           return '';
         }
@@ -235,7 +239,8 @@
         });
         if (index > -1) {
           const app = this.items[index];
-          app.images[image].publish = `${this.splitImage(image, 'image')}:${tag}`;
+          //app.images[image].publish = `${this.splitImage(image, 'image')}:${tag}`;
+          app.images[image].publish = `${this.splitImage(image + ':' + tag, 'image')}:${tag}`;
           this.$set(this.items, index, app);
         }
       },
